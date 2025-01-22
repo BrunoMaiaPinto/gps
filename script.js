a312c2066d49ac4188b2b8eb6456900e8517020f
Bruno <IEFPT103@ms.ciccopn.pt>
2025-01-22 16:45:54 +0000

    Initial Commit

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const KEY = "379fab09c36a4bbd88652d784b68d450";
 const btn = document.querySelector(".btn");
 
-async function getCodigoPostal(CP) {
-  const res = await fetch(`https://www.cttcodigopostal.pt/api/v1/${KEY}/${CP}`);
+async function getCodigoPostal(cp) {
+  const res = await fetch(`https://www.cttcodigopostal.pt/api/v1/${KEY}/${cp}`);
   const data = await res.json();
   // console.log(data);
   data.map(({ morada, freguesia, concelho, distrito, latitude, longitude }) => {
     document.querySelector(
       ".localizacao"
     ).innerHTML = `${morada}, ${freguesia}, ${concelho}, ${distrito},`;
-    console.log(
-      `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`
-    );
+
     const link = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
     document.querySelector(".gps").setAttribute("href", link);
     document.querySelector(".gps").innerHTML = "Google Maps";
@@ -22,3 +20,5 @@ async function getCodigoPostal(CP) {
 btn.addEventListener("click", () => {
   getCodigoPostal(document.getElementById("input").value);
 });
+
+document.getElementById("year").innerHTML = `${new Date().getFullYear()}`;
