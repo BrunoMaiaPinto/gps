e31db85981435c21b5f7e113b0711345228068b6
BrunoMaiaPinto <brunommpinto@sapo.pt>
2025-01-23 13:38:38 +0000

    Improve postal code validation and update location display

--- script.js
+++ script.js
@@ -5,19 +5,28 @@ async function getCodigoPostal(cp) {
   const res = await fetch(`https://www.cttcodigopostal.pt/api/v1/${KEY}/${cp}`);
   const data = await res.json();
   console.log(data);
-  data.map(({ morada, freguesia, concelho, distrito, latitude, longitude }) => {
-    document.querySelector(
-      ".localizacao"
-    ).innerHTML = `${morada}, ${freguesia}, ${concelho}, ${distrito},`;
+  if (data.length === 0) {
+    document.querySelector(".localizacao").innerHTML = "Código Postal Inválido";
+    return;
+  } else {
+    data.map(
+      ({ morada, freguesia, concelho, distrito, latitude, longitude }) => {
+        console.log(morada, freguesia, concelho, distrito, latitude, longitude);
+        document.querySelector(
+          ".localizacao"
+        ).innerHTML += `<p>${morada}, ${freguesia}, ${concelho}, ${distrito}</p>`;
 
-    const link = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
-    document.querySelector(".gps").setAttribute("href", link);
-    document.querySelector(".gps").innerHTML = "Google Maps";
-  });
-  return data;
+        const link = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+        document.querySelector(".gps").setAttribute("href", link);
+        document.querySelector(".gps").innerHTML = "Google Maps";
+      }
+    );
+    return data;
+  }
 }
 
 btn.addEventListener("click", () => {
+  document.querySelector(".localizacao").innerHTML = "";
   getCodigoPostal(document.getElementById("input").value);
 });
 
