e61802e0385d179ea17cf321cd8dd161f8ea2d3a
BrunoMaiaPinto <brunommpinto@sapo.pt>
2025-01-24 12:53:48 +0000

    Improve error handling in postal code fetch function and update invalid input messages

--- script.js
+++ script.js
@@ -2,24 +2,33 @@ const KEY = "379fab09c36a4bbd88652d784b68d450";
 const btn = document.querySelector(".btn");
 
 async function getCodigoPostal(cp) {
-  const res = await fetch(`https://www.cttcodigopostal.pt/api/v1/${KEY}/${cp}`);
-  const data = await res.json();
-  console.log(data);
-  if (data.length === 0) {
+  try {
+    const res = await fetch(
+      `https://www.cttcodigopostal.pt/api/v1/${KEY}/${cp}`
+    );
+    const data = await res.json();
+
+    console.log(data);
+    if (data.length === 0) {
+      document.querySelector(
+        ".localizacao"
+      ).innerHTML = `<p>Código Postal Inválido</p>`;
+      return;
+    } else {
+      data.map(
+        ({ morada, freguesia, concelho, distrito, latitude, longitude }) => {
+          const link = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+          document.querySelector(
+            ".localizacao"
+          ).innerHTML += `<p>${morada}, ${freguesia}, ${concelho}, ${distrito}. <a class='gps' href=${link} target="_blank">Google Maps</a></p>`;
+        }
+      );
+    }
+  } catch (error) {
     document.querySelector(
       ".localizacao"
-    ).innerHTML = `<p>Código Postal Inválido</p>`;
-    return;
-  } else {
-    data.map(
-      ({ morada, freguesia, concelho, distrito, latitude, longitude }) => {
-        const link = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
-        document.querySelector(
-          ".localizacao"
-        ).innerHTML += `<p>${morada}, ${freguesia}, ${concelho}, ${distrito}. <a class='gps' href=${link} target="_blank">Google Maps</a></p>`;
-      }
-    );
-    return data;
+    ).innerHTML = `<p>Formato Inválido</p>`;
+    console.error(error);
   }
 }
 
